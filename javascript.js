9d3401ed105376981bf5dbaa9371b3f7ce69c13b
Michael Dayo <Michaeldayo2703@gmail.com>
2022-03-26 12:36:01 +0800

    Rainbow Mode, Classic Button, Added Designs

--- javascript.js
+++ javascript.js
@@ -2,8 +2,12 @@
 const createDiv = document.createElement('div');
 const cloneDiv = createDiv.cloneNode();
 const canvasSelector = document.querySelector('.canvas-container');
+const buttonReset = document.querySelector('.reset');
 let slider = document.getElementById('resolution');
 let canvasChildren = canvasSelector.getElementsByTagName('div');
+const canvasChild = document.querySelectorAll(".canvas-container > div");
+let rainbow = document.querySelector('.rainbow');
+let classic = document.querySelector('.classic');
 
 // Create variable that creates more div elements and put it in a for loop.
 // Make that for loops default value to 16, but times it by the integer depending on the slider value.
@@ -13,12 +17,18 @@ let canvasChildren = canvasSelector.getElementsByTagName('div');
 // slider.addEventListener('input', () => {
 //     if (canvasSelector.hasChildNodes()) canvasReset()
 // }) // Does its job of deleting the tiles. Now we need to figure out how to add the other tiles.
-slider.addEventListener('input', canvasReset)
-slider.addEventListener('input', canvas)
-slider.addEventListener('input', classChange)
 
 canvas()
 classChange()
+
+slider.addEventListener('input', canvasReset)
+slider.addEventListener('input', canvas)
+slider.addEventListener('input', classChange)
+buttonReset.addEventListener('click', canvasReset)
+buttonReset.addEventListener('click', canvas)
+buttonReset.addEventListener('click', classChange)
+rainbow.addEventListener('click', rainbowMode)
+classic.addEventListener('click', classChange)
 
 function canvas() {
 
@@ -32,7 +42,7 @@ function canvas() {
 
 function canvasReset() {
     if (canvasSelector.hasChildNodes()) {
-        for (let i = canvasSelector.childElementCount;i >= canvasSelector.childElementCount; i--) {
+        for (let i = canvasSelector.childElementCount;i = canvasSelector.childElementCount; i--) {
             canvasSelector.removeChild(canvasSelector.lastChild)
         }        
     }
@@ -40,12 +50,53 @@ function canvasReset() {
 }  
 
 function classChange() {
-    for(let i = 0; i <= canvasSelector.childElementCount; i++ ) {
-        canvasChildren[i].addEventListener('mouseover', ()=> {
-            canvasChildren[i].style.background = 'red';
+    for(let div = 0; div < canvasChildren.length; div++) {
+        canvasChildren[div].addEventListener('mouseover', ()=> {
+            canvasChildren[div].style.background = 'red';
         })
-    }
+    };
+      
 }
+
+function rainbowMode() {
+    for(let div = 0; div < canvasChildren.length; div++) {
+        canvasChildren[div].addEventListener('mouseover', ()=> {
+            canvasChildren[div].style.background = colorRandomizer();
+        })
+    };
+      
+}
+
+
+
+function colorRandomizer() {
+    let color = "#";
+    let value = '0123456789ABCDEF';
+
+    for(let i=0 ; i<6; i++) {
+    color += value[Math.floor(Math.random()*16)];
+    };
+    return color
+}
+
+
+// function classicMode() {
+//     for (let i = 0;i <= canvasChildren.childElementCount; i++ ) {
+//         canvasChildren[i].addEventListener('mouseover', ()=>{
+//             canvasChildren[i].style.background = 'red';
+//         })
+// }
+
+
+
+// function rainbowMode() {
+//     for(let i = 0; i <= canvasSelector.childElementCount; i++ ) {
+//         canvasChildren[i].addEventListener('mouseover', ()=> {
+//             Math.floor(Math.random * 10)
+//             canvasChildren[i].style.background = 'red';
+//         })
+//     }
+// }
 
 
 
